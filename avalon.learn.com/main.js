f34ce06f5931eb9aa123b5c5b3497aa1b3d4aa0c
baiyu <baiyu@yiche.com>
2017-03-06 17:40:45 +0800

    avalon站点模拟构建

---
+++ avalon.learn.com/main.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="vendor/require/require.js" />
+
+require.config({
+    baseUrl: '',
+    paths: {
+        jquery: '',
+        avalon: '',
+        text: '',
+        domReady: ''
+    },
+    priority: ['text'],
+    shim: {
+        jquery: {
+            exports: "jQuery"
+        },
+        avalon: {
+            exports: "avalon"
+        }
+    }
+});
