b56ab1603c39b8153856a4e79dca808edd2cb6e3
baiyu <baiyu@yiche.com>
2017-03-07 11:17:41 +0800

    avalon部署样例

--- avalon.learn.com/main.js
+++ avalon.learn.com/main.js
@@ -1,12 +1,16 @@
 ﻿/// <reference path="vendor/require/require.js" />
+/// <reference path="vendor/jquery/jquery-3.1.1.min.js" />
+/// <reference path="vendor/require/text.js" />
+/// <reference path="vendor/require/domReady.js" />
+/// <reference path="vendor/avalon/avalon.js" />
 
 require.config({
     baseUrl: '',
     paths: {
-        jquery: '',
-        avalon: '',
-        text: '',
-        domReady: ''
+        jquery: 'vendor/jquery/jquery-3.1.1.min',
+        avalon: 'vendor/avalon/avalon',
+        text: 'vendor/require/text',
+        domReady: 'vendor/require/domReady'
     },
     priority: ['text'],
     shim: {
@@ -17,4 +21,21 @@ require.config({
             exports: "avalon"
         }
     }
+});
+
+require(['avalon', "domReady!"], function () {
+    avalon.log("加载avalon完毕，开始构建根vm与加载其他模块");
+
+    avalon.define({
+        $id: "root",
+        header: "这是根模块，用于放置其他模块都共用的东西，比如<b>用户名</b>什么的",
+        footer: "页脚消息",
+        page: "empty"
+    });
+
+    avalon.scan(document.body);
+
+    require(['./vendor/modules/aaa'], function () {
+        avalon.log("加载其他完毕");
+    });
 });
---
+++ avalon.learn.com/vendor/modules/aaa.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="../vendor/require/require.js" />
+
+
+define(['avalon', "text!./aaa.html"], function () {
+    avalon.define({
+        $id: "aaa",
+        username: "嘉靖皇帝"
+    });
+    avalon.vmodels.root.page = "aaa";
+});
