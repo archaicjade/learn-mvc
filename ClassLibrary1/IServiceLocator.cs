555be665059e607b482bde36b5a39e5282997c9c
baiyu <baiyu@yiche.com>
2017-04-21 17:49:45 +0800

    依赖注入学习

---
+++ ClassLibrary1/IServiceLocator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public interface IServiceLocator
+    {
+        object GetService(Type servcieType);
+    }
+
+    public interface IMessagingService
+    {
+        void SendMessage();
+    }
+
+    public class NotificationSystem
+    {
+        public IMessagingService MessageService
+        {
+            get; set;
+        }
+
+
+        public void InterestingEventHappened()
+        {
+            if (MessageService == null)
+            {
+                throw new NullReferenceException("please set messageservice before calling");
+            }
+
+            MessageService.SendMessage();
+        }
+    }
+
+    public static class ServiceLocatorExtensions
+    {
+        public static TService GetService<TService>(this IServiceLocator locator)
+        {
+            return (TService)locator.GetService(typeof(TService));
+        }
+    }
+}
